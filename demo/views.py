0ac17deb6a8d102cee7c0f80a6a38f74dc7da237
sone <sthaya@gmail.com>
2021-04-25 19:02:11 -0700

    created first temp for templates

--- demo/views.py
+++ demo/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
+from django.shortcuts import render
 
 # Create your views here.
-class Another(View):
+def first(request):
+    return render(request, 'first_temp.html')
+
+
+
+# class Another(View):
     
-    book = Book.objects.get(id=2)
-    output = f"We have {book.title} book with ID {book.id}.<br>"
+#     book = Book.objects.get(id=2)
+#     output = f"We have {book.title} book with ID {book.id}.<br>"
     
-    #for book in books:
-    #    output += f"We have {book.title} book with ID {book.id} in DB.<br>"
+#     #for book in books:
+#     #    output += f"We have {book.title} book with ID {book.id} in DB.<br>"
     
-    def get(self, request):
-        return HttpResponse(self.output)
+#     def get(self, request):
+#         return HttpResponse(self.output)
     
 
-def first(request):
-    return HttpResponse('First message from views')
