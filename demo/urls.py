d98c49aabad26653fd28677384fab109e5c489d3
sone <sthaya@gmail.com>
2021-04-26 20:03:19 -0700

    serializers

--- demo/urls.py
+++ demo/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
+from .views import BookViewSet
 # from .views import Another
 
+router = routers.DefaultRouter()
+router.register('books', BookViewSet)
+
 urlpatterns = [
-    path('first', views.first),
+    path('', include(router.urls)),
     # path('another', Another.as_view()),
 ]
