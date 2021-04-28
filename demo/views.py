a12919d7eefa0478726cd83b4ead76f19b8a2322
sone <sthaya@gmail.com>
2021-04-27 19:54:19 -0700

    setup permissions

--- demo/views.py
+++ demo/views.py
@@ -6,6 +6,8 @@
 from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 # def first(request):
@@ -16,6 +18,8 @@ from .models import Book
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
 
 
